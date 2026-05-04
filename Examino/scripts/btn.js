7343cd2ee89054a210b2c78e0eaf3ac0898ce4b7
Milad Kahrabi <155329122+m1lawd@users.noreply.github.com>
2026-05-04 20:40:09 +0330

    "input created / back btn created / styling left"

--- Examino/scripts/btn.js
+++ Examino/scripts/btn.js
@@ -21,3 +21,15 @@ document.addEventListener("DOMContentLoaded", function () {
     window.location.href = "sign-up.html";
   });
 });
+
+// منتظر می‌مونیم تا صفحه کامل لود بشه
+document.addEventListener("DOMContentLoaded", function () {
+  // دکمه ورود رو پیدا می‌کنیم
+  const loginBtn = document.getElementById("back");
+
+  // وقتی روی دکمه کلیک شد
+  loginBtn.addEventListener("click", function () {
+    // به صفحه login.html برو
+    window.location.href = "home.html";
+  });
+});
