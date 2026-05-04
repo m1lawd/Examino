83ad25bb85b976a49ee61ca91abd36c71f15f42f
Milad Kahrabi <155329122+m1lawd@users.noreply.github.com>
2026-05-04 20:27:46 +0330

    "sing-up btn activated"

--- Examino/scripts/btn.js
+++ Examino/scripts/btn.js
@@ -13,11 +13,11 @@ document.addEventListener("DOMContentLoaded", function () {
 // منتظر می‌مونیم تا صفحه کامل لود بشه
 document.addEventListener("DOMContentLoaded", function () {
   // دکمه ورود رو پیدا می‌کنیم
-  const loginBtn = document.getElementById("log-in");
+  const loginBtn = document.getElementById("sign-in");
 
   // وقتی روی دکمه کلیک شد
   loginBtn.addEventListener("click", function () {
     // به صفحه login.html برو
-    window.location.href = "login.html";
+    window.location.href = "sign-up.html";
   });
 });
