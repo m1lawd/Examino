c8b49806d8402495ac450bcdeabc71c46a21bc01
Milad Kahrabi <155329122+m1lawd@users.noreply.github.com>
2026-05-04 20:26:34 +0330

    "colors.css craeted on a new file and imported to the whole css file"

--- Examino/scripts/btn.js
+++ Examino/scripts/btn.js
@@ -9,3 +9,15 @@ document.addEventListener("DOMContentLoaded", function () {
     window.location.href = "login.html";
   });
 });
+
+// منتظر می‌مونیم تا صفحه کامل لود بشه
+document.addEventListener("DOMContentLoaded", function () {
+  // دکمه ورود رو پیدا می‌کنیم
+  const loginBtn = document.getElementById("log-in");
+
+  // وقتی روی دکمه کلیک شد
+  loginBtn.addEventListener("click", function () {
+    // به صفحه login.html برو
+    window.location.href = "login.html";
+  });
+});
